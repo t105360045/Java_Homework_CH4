c957c02a0719498f7151e3c682a5a1cf581bedc7
Your Name <you@example.com>
2018-11-08 15:37:00 +0800

    123

--- test_p14/src/test_p14/Sample4_5.java
+++ test_p14/src/test_p14/Sample4_5.java
@@ -1,45 +1,43 @@
-package test_p11;
+package test_p14;
 
-public class Sample4_4 
+public class Sample4_5 
 {
 	public static void main(String[] args)
 	{
-		RacingCar rccar1;
-		rccar1 = new RacingCar();
-	
-		rccar1.setCar(1234,20.5);
-		rccar1.setCourse(5);
-	
-		rccar1.show();
-
+		Car car1;
+		car1 = new RacingCar();
+		
+		car1.setCar(1234, 20.5);
+		
+		car1.show();
 	}
-	
-}
 
+}
 class Car
 {
 	protected int num;
 	protected double gas;
 	
-	public Car() 
+	public Car()
 	{
 		num = 0;
 		gas = 0.0;
-		System.out.println("產生了車子");
-		
+		System.out.println("生產了車子");
 	}
 	public void setCar(int n,double g)
 	{
 		num = n;
 		gas = g;
-		
 		System.out.println("將車號設為"+num+"，汽油量設為"+gas);
+		
 	}
+	
 	public void show()
 	{
-		System.out.print("車號是"+num);
-		System.out.print("汽油量是"+gas);
+		System.out.println("車號是"+num);
+		System.out.println("汽油量是"+gas);
 	}
+
 }
 
 class RacingCar extends Car
@@ -51,6 +49,7 @@ class RacingCar extends Car
 		course = 0;
 		System.out.println("生產了賽車");
 	}
+	
 	public void setCourse(int c)
 	{
 		course = c;
@@ -62,7 +61,6 @@ class RacingCar extends Car
 		System.out.println("賽車的車號是"+num);
 		System.out.println("汽油量是"+gas);
 		System.out.println("賽車編號是"+course);
-		
-		
 	}
+	
 }
--- test_p17/src/test_p17/Sample4_6.java
+++ test_p17/src/test_p17/Sample4_6.java
@@ -1,19 +1,25 @@
-package test_p11;
+package test_p17;
 
-public class Sample4_4 
+public class Sample4_6 
 {
 	public static void main(String[] args)
 	{
-		RacingCar rccar1;
-		rccar1 = new RacingCar();
-	
-		rccar1.setCar(1234,20.5);
-		rccar1.setCourse(5);
-	
-		rccar1.show();
-
+		Car cars[];
+		cars = new Car[2];
+		
+		cars[0] = new Car();
+		cars[0].setCar(1234, 20.5);
+		
+		cars[1] = new RacingCar();
+		cars[1].setCar(4567, 30.5);
+		
+		for(int i=0;i<cars.length;i++)
+		{
+			cars[i].show();
+		}
 	}
 	
+
 }
 
 class Car
@@ -21,24 +27,23 @@ class Car
 	protected int num;
 	protected double gas;
 	
-	public Car() 
+	public Car()
 	{
 		num = 0;
 		gas = 0.0;
-		System.out.println("產生了車子");
-		
+		System.out.println("生產了車子");
 	}
+	
 	public void setCar(int n,double g)
 	{
 		num = n;
 		gas = g;
-		
 		System.out.println("將車號設為"+num+"，汽油量設為"+gas);
 	}
 	public void show()
 	{
-		System.out.print("車號是"+num);
-		System.out.print("汽油量是"+gas);
+		System.out.println("車號是"+num);
+		System.out.println("汽油量是"+gas);
 	}
 }
 
@@ -56,13 +61,10 @@ class RacingCar extends Car
 		course = c;
 		System.out.println("將賽車編號設為"+course);
 	}
-	
 	public void show()
 	{
 		System.out.println("賽車的車號是"+num);
 		System.out.println("汽油量是"+gas);
 		System.out.println("賽車編號是"+course);
-		
-		
 	}
 }
